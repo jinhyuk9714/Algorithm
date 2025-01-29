ff0539ef9be1ed3fd23173ef7f9fc979d0421d21
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-01-29 14:01:53 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.56 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,49 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int answer = 0, count = 0, sum = 0;
-        int p, q;
+        int[] arr = {a, b, c, d};
+        Arrays.sort(arr);
 
-        if (a==b){
-            sum += a;
-            count++;
-        }
-        if (a==c){
-            sum += a;
-            count++;
-        }
-        if (a==d){
-            sum += a;
-            count++;
-        }
-        if (b==c){
-            sum += b;
-            count++;
-        }
-        if (b==d){
-            sum += b;
-            count++;
-        }
-        if (c==d){
-            sum += c;
-            count++;
-        }
+        if (arr[0] == arr[3]) return 1111 * arr[0];
 
-        if (count == 6) {
-            p = sum / 6;
-            answer = 1111 * p;
-        } else if (count == 3) {
-            p = sum / 3;
-            q = a + b + c + d - sum;
-            answer = (int) Math.pow(10 * p + q, 2);
-        } else if (count == 2) {
-            p = sum - a;
-            q = a;
-            answer = (p + q) * Math.abs(p - q);
-        } else if (count == 1) {
-            answer = (a * b * c * d) / ((int) Math.pow(sum, 2));
-        } else {
-            answer = Math.min(Math.min(a, b), Math.min(c, d));
-        }
-        return answer;
+        if (arr[0] == arr[2]) return (int) Math.pow(10 * arr[0] + arr[3], 2);
+        if (arr[1] == arr[3]) return (int) Math.pow(10 * arr[1] + arr[0], 2);
+
+        if (arr[0] == arr[1] && arr[2] == arr[3]) return (arr[0] + arr[2]) * Math.abs(arr[0] - arr[2]);
+
+        if (arr[0] == arr[1]) return arr[2] * arr[3];
+        if (arr[1] == arr[2]) return arr[0] * arr[3];
+        if (arr[2] == arr[3]) return arr[0] * arr[1];
+
+        return arr[0];
     }
 }
