9a50d5a5ecae020a5c8b54ee705ecc14a805ace2
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-02-01 07:24:25 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 68.44 ms, Memory: 145 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] arr) {
+        int count = 0;
+
+        while (true) {
+            int[] temp = Arrays.copyOf(arr, arr.length);
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] /= 2;
+                } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+                    arr[i] = arr[i] * 2 + 1;
+                }
+            }
+            if (Arrays.equals(temp, arr)) {
+                break;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
