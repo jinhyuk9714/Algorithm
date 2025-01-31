8fa17c273e4ccab126e6b313846a3149cd734205
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-02-01 06:05:50 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,13 +1,10 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] part1 = Arrays.copyOfRange(num_list, n, num_list.length);
-        int[] part2 = Arrays.copyOfRange(num_list, 0, n);
+        int[] result = new int[num_list.length];
 
-        int[] result = new int[part1.length + part2.length];
-
-        System.arraycopy(part1, 0, result, 0, part1.length);
-        System.arraycopy(part2, 0, result, part1.length, part2.length);
+        System.arraycopy(num_list, n, result, 0, num_list.length - n);
+        System.arraycopy(num_list, 0, result, num_list.length - n, n);
 
         return result;
     }
