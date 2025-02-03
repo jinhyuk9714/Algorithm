3588b92803ec87217ebf43708683b9b30708b571
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-02-03 17:11:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        String str = "" + n;
         int answer = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            answer += (str.charAt(i) - '0');
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
