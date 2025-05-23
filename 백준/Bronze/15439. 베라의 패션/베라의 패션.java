9c599d70d1573ea17026809f1f5d69831b8d7fd8
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-05-23 10:47:56 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(N * (N - 1));
+        System.out.print(sb);
+    }
+}
