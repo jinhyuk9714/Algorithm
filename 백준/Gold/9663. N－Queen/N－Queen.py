0bed2ecd80d18c2d3185f34e7a72817f4e31af65
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-07-30 15:39:38 +0900

    [Gold IV] Title: N-Queen, Time: 19024 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,20 @@
 n = int(input())
-chess_map = [0] * n  # chess_map[row] = col: row행에 col열에 퀸을 뒀다는 의미
 result = 0
 
+col_used = [False] * n
+diag1 = [False] * (2 * n)  # ↘: row + col
+diag2 = [False] * (2 * n)  # ↙: row - col + n
 
-def is_safe(row):
-    for i in range(row):
-        # 같은 열이거나, 같은 ↘ / ↙ 대각선에 퀸이 있다면 False
-        if (chess_map[i] == chess_map[row]) or \
-           (abs(chess_map[i] - chess_map[row]) == abs(i - row)):
-            return False
-    return True
-
-
-def queen(row):
+def dfs(row):
     global result
     if row == n:
         result += 1
         return
     for col in range(n):
-        chess_map[row] = col
-        if is_safe(row):
-            queen(row + 1)  # 다음 행으로 진행
+        if not col_used[col] and not diag1[row + col] and not diag2[row - col + n]:
+            col_used[col] = diag1[row + col] = diag2[row - col + n] = True
+            dfs(row + 1)
+            col_used[col] = diag1[row + col] = diag2[row - col + n] = False
 
-
-queen(0)
+dfs(0)
 print(result)
