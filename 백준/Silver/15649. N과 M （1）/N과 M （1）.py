5bca77f2c0dbae88ccb1e7166772ce64f0337e19
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-09-09 14:00:23 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 36744 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,9 +1,27 @@
-from itertools import permutations
+import sys
 
-n, m = map(int, input().split())
-data = list(range(1, n + 1))
+input = sys.stdin.readline
+write = sys.stdout.write
 
-result = list(permutations(data, m))
+N, M = map(int, input().split())
 
-for i in result:
-    print(*i)
+path = []
+visited = [False] * (N + 1)
+result = []
+
+
+def dfs(depth):
+    if depth == M:
+        result.append(' '.join(map(str, path)))
+        return
+    for i in range(1, N + 1):
+        if visited[i] == False:
+            visited[i] = True
+            path.append(i)
+            dfs(depth + 1)
+            path.pop()
+            visited[i] = False
+
+
+dfs(0)
+write('\n'.join(result))
