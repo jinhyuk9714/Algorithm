92357811c917e085cef68265ab7a997e7a8c096c
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-09-09 14:39:51 +0900

    [Silver III] Title: N과 M (3), Time: 916 ms, Memory: 113672 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,14 +1,23 @@
-n, m = map(int, input().split())
+import sys
+
+input = sys.stdin.readline
+write = sys.stdout.write
+
+N, M = map(int, input().split())
+
 path = []
+result = []
 
-def dfs():
-    if len(path) == m:
-        print(*path)
+
+def dfs(depth):
+    if depth == M:
+        result.append(' '.join(map(str, path)))
         return
-
-    for i in range(1, n + 1):
+    for i in range(1, N + 1):
         path.append(i)
-        dfs()
+        dfs(depth + 1)
         path.pop()
 
-dfs()
+
+dfs(0)
+write('\n'.join(result))
