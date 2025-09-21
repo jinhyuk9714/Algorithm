0fa30bdde72f50b79e0a9e72eb653eecdef5b0b7
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2025-09-21 20:25:47 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,16 +1,8 @@
 import sys
+from itertools import accumulate
 
 input = sys.stdin.readline
 
 N = int(input())
-orders = list(map(int, input().split()))
-orders.sort()
-
-p = 0
-sum = 0
-
-for i in range(N):
-    p += orders[i]
-    sum += p
-
-print(sum)
+orders = sorted(map(int, input().split()))
+print(sum(accumulate(orders)))
