573ab1825a3ed8ccefbc73ba723b5af3ff53fc14
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2026-01-02 14:34:46 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 208 ms, Memory: 49336 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -2,19 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 
-A = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-# 누적합 P[i] = A[0] ~ A[i-1]의 합 (길이 N+1, P[0]=0)
-P = [0] * (N + 1)
-for i in range(1, N + 1):
-    P[i] = P[i - 1] + A[i - 1]
+prefix = [0] * (n + 1)
 
-# M개의 질의 (i, j): i~j 합 출력 (1-indexed)
+for i in range(1, n + 1):
+   prefix[i] = prefix[i - 1] + arr[i - 1]
+
 out = []
-for _ in range(M):
+for _ in range(m):
     i, j = map(int, input().split())
-    out.append(str(P[j] - P[i - 1]))
 
-print('\n'.join(out))
+    out.append(str(prefix[j] - prefix[i - 1]))
+
+print("\n".join(out))
