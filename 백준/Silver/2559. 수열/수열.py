31bf424775eb9e0ac17b6e3843f94febe0c616b0
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2026-01-02 16:19:25 +0900

    [Silver III] Title: 수열, Time: 68 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -2,16 +2,28 @@ import sys
 
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-A = list(map(int, input().split()))
+n, k = map(int, input().split())
 
-window = sum(A[:K])
-ans = window
+arr = list(map(int, input().split()))
 
-# 한 칸씩 미는 동안: 새로 들어온 원소 더하고, 빠진 원소 빼기
-for i in range(K, N):
-    window += A[i] - A[i - K]
-    if window > ans:
-        ans = window
+cur = sum(arr[:k])
+best = cur
 
-print(ans)
+for i in range(k, n):
+    cur += arr[i] - arr[i - k]
+    if cur > best:
+        best = cur
+
+print(best)
+
+# # 누적합
+# prefix = [0] * (n + 1)
+#
+# for i in range(1, n + 1):
+#     prefix[i] = prefix[i - 1] + arr[i - 1]
+#
+# out = []
+# for i in range(k, n + 1):
+#     out.append(str(prefix[i] - prefix[i - k]))
+#
+# print("\n".join(out))
