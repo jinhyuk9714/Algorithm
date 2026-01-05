7587d4613dc841a7a43d7b6679eeaf2c2c789727
jinhyuk9714 <163989462+jinhyuk9714@users.noreply.github.com>
2026-01-05 16:58:45 +0900

    [Gold IV] Title: 연구소, Time: 1408 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -5,13 +5,12 @@ from itertools import combinations
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
 original_map = [list(map(int, input().split())) for _ in range(n)]
 
 empty = []
 virus = []
 
-# 0, 2 위치 구하기
+# 1. 초기 좌표 저장
 for i in range(n):
     for j in range(m):
         if original_map[i][j] == 0:
@@ -21,44 +20,40 @@ for i in range(n):
 
 
 def spread_virus(board):
-    q = deque(virus)  # 처음에 찾아둔 바이러스 좌표 다 넣기
-
+    q = deque(virus)
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
     while q:
         x, y = q.popleft()
-
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-
-            # 맵 범위 안이고, 빈칸(0)이라면 바이러스 퍼짐
             if 0 <= nx < n and 0 <= ny < m:
                 if board[nx][ny] == 0:
-                    board[nx][ny] = 2  # 감염!
+                    board[nx][ny] = 2
                     q.append((nx, ny))
 
 
 def get_safe_area(board):
-    global sum_res
-
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 0:
-                sum_res += 1
+    # 피드백 2: 파이썬 스타일로 0 개수 세기
+    return sum(row.count(0) for row in board)
 
 
 res = 0
+
+# 2. 벽 3개 세우기 (조합)
 for walls in combinations(empty, 3):
+    # 맵 복사
     temp_map = [row[:] for row in original_map]
 
+    # 벽 세우기
     for r, c in walls:
         temp_map[r][c] = 1
 
+    # 바이러스 퍼뜨리기
     spread_virus(temp_map)
 
-    sum_res = 0
-    get_safe_area(temp_map)
-    res = max(res, sum_res)
+    # 피드백 1: 리턴값 받아서 갱신
+    res = max(res, get_safe_area(temp_map))
 
 print(res)
